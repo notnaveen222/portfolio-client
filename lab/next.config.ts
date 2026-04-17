49ec7c0ba2f80d25759ec0584309380be61ab142
naveen <notnaveen2@gmail.com>
2026-04-17 10:02:23 +0530

    update2

--- lab/next.config.ts
+++ lab/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
