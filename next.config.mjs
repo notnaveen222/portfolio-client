1ace5346e53c3bdc913b1b8b64a5f803408ad11d
naveen <notnaveen2@gmail.com>
2026-07-20 20:48:49 +0530

    C:/Program Files/Git/resume page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination:
+          "https://drive.google.com/file/d/1vRQgae6LcKgin28Vg5FSFrWLW12Vo0Yq/view?usp=sharing",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
