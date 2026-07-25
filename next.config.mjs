f17f32a3afc9a040e00533ef60161d03bf59352e
naveen <notnaveen2@gmail.com>
2026-07-25 12:58:26 +0530

    updated resume

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: "/resume",
         destination:
-          "https://drive.google.com/file/d/1vRQgae6LcKgin28Vg5FSFrWLW12Vo0Yq/view?usp=sharing",
+          "https://drive.google.com/file/d/1tTQjUPWnbUTACKFlO9FGWBsWbdr1VVFu/view?usp=sharing",
         permanent: false,
       },
     ];
